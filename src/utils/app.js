915538488e171d135103e10b22823dff1089a34d
Kamila <kamillkaa95@gmail.com>
2025-09-18 17:21:30 +0300

    pino add

--- src/utils/app.js
+++ src/utils/app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import pino from 'pino';
 import pinoHttp from 'pino-http';
 import { contactsRouter } from '../routers/contacts.js';
 import { notFoundHandler } from '../middlewares/notFoundHandler.js';
@@ -8,9 +9,21 @@ import { errorHandler } from '../middlewares/errorHandler.js';
 export const createApp = () => {
   const app = express();
 
+  const logger = pino({
+    transport: {
+      target: 'pino-pretty',
+      options: {
+        colorize: true,
+        singleLine: false,
+        translateTime: 'SYS:HH:mm:ss',
+        ignore: 'pid,hostname',
+      },
+    },
+  });
+
   app.use(cors());
   app.use(express.json());
-  app.use(pinoHttp({}));
+  app.use(pinoHttp({ logger }));
 
   app.use('/contacts', contactsRouter);
 
